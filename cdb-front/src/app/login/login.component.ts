70845ce0727163f040d30a578613df15c00fc9c4
excilys <victor.bonneau27@gmail.com>
2019-11-29 16:55:52 +0100

    login page

---
+++ cdb-front/src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm:FormGroup;
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.loginForm=this.fb.group({
+      username:["",Validators.required],
+      password:["",Validators.required]
+    })
+  }
+
+}
