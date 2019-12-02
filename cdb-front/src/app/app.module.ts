46dbf9e3b7edae7accf9fea92cb21b11e25f7a3a
huliop <baptistesaladin@gmail.com>
2019-12-02 14:27:24 +0100

    Version 1.0

--- cdb-front/src/app/app.module.ts
+++ cdb-front/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import { GridModule, PageService, SortService, FilterService, EditService } from '@syncfusion/ej2-angular-grids';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {TestComponent} from './test/test.component';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    GridModule
   ],
-  providers: [],
+  providers: [
+    PageService, 
+    SortService,
+    FilterService,
+    EditService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
