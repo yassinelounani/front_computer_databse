e01e8cc1598039b7222922c2e0ecfa0a62ca9a3d
yassine lounani <yas.lounani@gmail.com>
2019-12-02 16:03:15 +0100

    add page login

--- cdb-front/src/app/app-routing.module.ts
+++ cdb-front/src/app/app-routing.module.ts
@@ -7,18 +7,18 @@ import { CompaniesComponent } from './companies/companies.component';
 
 const routes: Routes = [
   {
-    path:'login',
-    component:LoginComponent,
+    path: 'login',
+    component: LoginComponent,
   },
   {
-    path:'computers',
-    component:ComputersComponent,
-    canActivate:[ IsSignedInService ]
+    path: 'computers',
+    component: ComputersComponent,
+    canActivate: [ IsSignedInService ]
   },
   {
-    path:'companies',
-    component:CompaniesComponent,
-    canActivate:[ IsSignedInService]
+    path: 'companies',
+    component: CompaniesComponent,
+    canActivate: [ IsSignedInService]
   }
 ];
 
--- cdb-front/src/app/app.component.ts
+++ cdb-front/src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ComputerService } from './service/computer.service';
-import { Computer } from './model/computer.model';
-import {PageSettingsModel, EditSettingsModel } from '@syncfusion/ej2-angular-grids';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
-  
+export class AppComponent {
 }
