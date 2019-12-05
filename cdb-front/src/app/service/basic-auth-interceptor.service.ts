3522c62915ed2bf0ee9a76e664aee012b3d9fc5b
huliop <baptistesaladin@gmail.com>
2019-12-05 17:43:51 +0100

    add companies list fix

--- cdb-front/src/app/service/basic-auth-interceptor.service.ts
+++ cdb-front/src/app/service/basic-auth-interceptor.service.ts
@@ -8,18 +8,14 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private userService: UserService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler) {
-    console.log(sessionStorage.getItem('username'))
     if (sessionStorage.getItem('username') && sessionStorage.getItem('token') != null ) {
-      console.log('entring to handling');
       const headers = new HttpHeaders({
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
         'Authorization': `${sessionStorage.getItem('token')}`
       });
-      console.log(headers);
       request = request.clone({headers});
     }
-    console.log('handling request');
     return next.handle(request);
   }
 }
