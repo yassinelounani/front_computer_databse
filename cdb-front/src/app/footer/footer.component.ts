c185722ea535f55d9d9caeaeefb11ba07230a564
yassine lounani <yas.lounani@gmail.com>
2019-12-10 10:30:14 +0100

    ignoring footer and header for page login

--- cdb-front/src/app/footer/footer.component.ts
+++ cdb-front/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
